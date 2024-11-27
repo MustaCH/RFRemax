33781431a074ad61f2a6cd6f6ecee647a4e87727
ignacio poletti <ip@discoverit.store>
2024-11-27 10:59:33 -0300

    STYLE | Modified contact section heropic

--- app/components/whoami-card/index.tsx
+++ app/components/whoami-card/index.tsx
@@ -1,7 +1,7 @@
 export const WhoamiCard = () => {
     return (
         <div className="flex flex-col gap-8 md:gap-0 md:flex-row justify-evenly items-center">
-            <div className="rounded-full border-4 border-white bg-cover bg-[center_bottom] shadow-lg bg-[url(https://res.cloudinary.com/dfuru6l6d/image/upload/v1732294268/Romina-04_fpifqe.jpg)] w-80 h-80 md:w-96 md:h-96">
+            <div className="rounded-full border-4 border-white bg-[length:35.5rem_50rem] bg-[center_top_-16rem] shadow-lg bg-[url(https://res.cloudinary.com/dfuru6l6d/image/upload/v1732294268/Romina-04_fpifqe.jpg)] w-80 h-80 md:w-96 md:h-96">
             </div>
             <div className="flex flex-col gap-4 bg-white rounded-lg p-8 w-96 shadow-lg">
                 <div className="border-b border-b-[#712536] pb-2">
