9df2f5dbab30ce44cd4c5a8e58ef7284576f4a69
ignacio poletti <ip@discoverit.store>
2024-11-28 02:01:43 -0300

    FEATURE | Modified .env

--- app/components/contact-form-modal/index.tsx
+++ app/components/contact-form-modal/index.tsx
@@ -57,7 +57,7 @@ export const ContactFormModal: FC<ContactFormModalProps> = ({ onClose }) => {
       >
         <button
           onClick={startExitAnimation}
-          className="absolute top-2 right-2 text-gray-500 hover:text-gray-800"
+          className="absolute top-2 right-4 text-xl text-gray-500 hover:text-gray-800"
         >
           ✖
         </button>
