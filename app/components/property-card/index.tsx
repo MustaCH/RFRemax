36a7490e44851db951fcc096108f304bd63ae656
Ignacio Nicolás García Poletti <polettiignacio7@gmail.com>
2025-04-14 17:59:28 -0300

    FEATURE | Added availability property to PropertyCard and updated PropertiesContainer to display availability status

--- app/components/property-card/index.tsx
+++ app/components/property-card/index.tsx
@@ -1,43 +1,75 @@
-'use client'
+"use client";
 
-import { LocationType, OperationType, PriceType } from "@/app/types"
-import { PriceFormatter } from "@/app/utils"
-import Link from "next/link"
-import { FC } from "react"
+import { LocationType, OperationType, PriceType } from "@/app/types";
+import { PriceFormatter } from "@/app/utils";
+import Link from "next/link";
+import { FC } from "react";
 
 interface PropertyCardProps {
-    id: string,
-    operation: OperationType,
-    image: string,
-    price: PriceType,
-    expenses?: PriceType,
-    title: string,
-    location?: LocationType
+  id: string;
+  operation: OperationType;
+  availability: boolean;
+  image: string;
+  price: PriceType;
+  expenses?: PriceType;
+  title: string;
+  location?: LocationType;
 }
 
-export const PropertyCard: FC<PropertyCardProps> = ({image, operation, price, expenses, title, id, location}) => {
-    return (
-        <Link href={`${id}`} className="shadow-md rounded-lg border-2 border-transparent hover:border-[#B0BBC5] duration-150 bg-white w-96">
-            <div style={{ backgroundImage: `url(${image})` }} className="h-60 bg-center bg-cover bg-no-repeat w-full rounded-t-lg"></div>
-            <div className="flex flex-col gap-2 px-6 pb-6 pt-3">
-                <div className="border border-[#B0BBC5] rounded-lg p-1 w-fit text-xs">
-                    <p>{operation.type === 'SALE' ? 'Venta' : 'Alquiler'}</p>
-                </div>
-                <div>
-                    <PriceFormatter className="font-semibold text-lg" value={price}/>
-                    {expenses && <PriceFormatter title="Expensas:" className="text-xs font-light" value={expenses}/>}
-                </div>
-                <div>
-                    <h3 className="text-xs md:text-sm">{title}</h3>
-                </div>
-                <div className="flex gap-1 text-xs mt-4">
-                    {location?.province && <p>{location?.province} /</p>}
-                    <p>{location?.city} {location?.hood && "/"}</p>
-                    <p>{location?.hood}</p>
-                </div>
-            </div>
-        </Link>
-    )
-}
+export const PropertyCard: FC<PropertyCardProps> = ({
+  image,
+  operation,
+  availability,
+  price,
+  expenses,
+  title,
+  id,
+  location,
+}) => {
+  return (
+    <Link
+      href={`${id}`}
+      className="shadow-md rounded-lg border-2 border-transparent hover:border-[#B0BBC5] duration-150 bg-white w-96"
+    >
+      <div
+        style={{ backgroundImage: `url(${image})` }}
+        className="relative h-60 bg-center bg-cover bg-no-repeat w-full rounded-t-lg"
+      >
+        {availability === false && (
+          <div className="absolute top-0 right-0 p-2 bg-red-700 rounded-tr-lg rounded-bl-lg">
+            <p className="text-white">
+              {operation.type === "SALE" ? "Vendido" : "Alquilado"}
+            </p>
+          </div>
+        )}
+      </div>
+      <div className="flex flex-col gap-2 px-6 pb-6 pt-3">
+        <div className="border border-[#B0BBC5] rounded-lg p-1 w-fit text-xs">
+          <p>{operation.type === "SALE" ? "Venta" : "Alquiler"}</p>
+        </div>
+        <div>
+          <PriceFormatter className="font-semibold text-lg" value={price} />
+          {expenses && (
+            <PriceFormatter
+              title="Expensas:"
+              className="text-xs font-light"
+              value={expenses}
+            />
+          )}
+        </div>
+        <div>
+          <h3 className="text-xs md:text-sm">{title}</h3>
+        </div>
+        <div className="flex gap-1 text-xs mt-4">
+          {location?.province && <p>{location?.province} /</p>}
+          <p>
+            {location?.city} {location?.hood && "/"}
+          </p>
+          <p>{location?.hood}</p>
+        </div>
+      </div>
+    </Link>
+  );
+};
 
 export default PropertyCard;
--- app/containers/properties-container/index.tsx
+++ app/containers/properties-container/index.tsx
@@ -1,24 +1,31 @@
-import { PropertyCard } from "@/app/components"
-import { FaHouse } from "react-icons/fa6"
-import { projects } from "../../constants"
+import { PropertyCard } from "@/app/components";
+import { FaHouse } from "react-icons/fa6";
+import { projects } from "../../constants";
 
 export const PropertiesContainer = () => {
-    
-    return (
-        <div className="flex flex-col gap-8 p-12 md:p-24">
-                <div className="flex items-center gap-2 text-xl text-[#3B4352] border-b border-b-[#4088e3] pb-4">
-                    <FaHouse />
-                    <h2 className="capitalize font-medium">propiedades</h2>
-                </div>
-                <div className="flex flex-col md:flex-row gap-8 items-center">
-                    {
-                        projects.map((p, index) => (
-                            <PropertyCard key={index} id={p.id} title={p.title} operation={p.operation} price={p.price} expenses={p.expenses} image={p.images[0]} location={p.location}  />
-                        ))
-                    }
-                </div>
-            </div>
-    )
-}
+  return (
+    <div className="flex flex-col gap-8 p-12 md:p-24">
+      <div className="flex items-center gap-2 text-xl text-[#3B4352] border-b border-b-[#4088e3] pb-4">
+        <FaHouse />
+        <h2 className="capitalize font-medium">propiedades</h2>
+      </div>
+      <div className="flex flex-col md:flex-row gap-8 items-center">
+        {projects.map((p, index) => (
+          <PropertyCard
+            key={index}
+            id={p.id}
+            title={p.title}
+            operation={p.operation}
+            price={p.price}
+            expenses={p.expenses}
+            image={p.images[0]}
+            location={p.location}
+            availability={p.availability}
+          />
+        ))}
+      </div>
+    </div>
+  );
+};
 
 export default PropertiesContainer;
