2fab4e74d6698b19c803d5bfa023ab600e34fda2
Ignacio Nicolás García Poletti <polettiignacio7@gmail.com>
2025-04-14 18:03:52 -0300

    FEATURE | Enhanced PropertyInfo component to display availability status for properties

--- app/components/propety-info/index.tsx
+++ app/components/propety-info/index.tsx
@@ -1,25 +1,40 @@
-import { IProjectType } from "@/app/types"
+import { IProjectType } from "@/app/types";
 import { PriceFormatter } from "@/app/utils";
-import { FC } from "react"
+import { FC } from "react";
 
 interface PropertyInfoProps {
-    project?: IProjectType;
+  project?: IProjectType;
 }
 
-export const PropertyInfo: FC<PropertyInfoProps> = ({project}) => {
-    return (    
-        <div className="flex flex-col gap-6">     
-            <h1 className="text-xl md:text-3xl">{project?.title}</h1>
-            <div>
-            <div className="text-sm border border-[#B0BBC5] bg-[#3B4352] text-white py-1 px-2 w-fit rounded-lg mb-2">
-                {project?.operation.type === 'SALE' ? 'Venta' : project?.operation.type === 'RENT' ? 'Alquiler' : ''}
-            </div>
-            <PriceFormatter className="text-4xl" value={project?.price}/>
-            {project?.expenses && <PriceFormatter className="text-sm" value={project?.expenses} title="Expensas:"/>}
-            </div>
-        </div>
-
-    )
-}
+export const PropertyInfo: FC<PropertyInfoProps> = ({ project }) => {
+  return (
+    <div className="flex flex-col gap-6">
+      <h1 className="text-xl md:text-3xl">{project?.title}</h1>
+      <div>
+        {project?.availability === true ? (
+          <div className="text-sm border border-[#B0BBC5] bg-[#3B4352] text-white py-1 px-2 w-fit rounded-lg mb-2">
+            {project?.operation.type === "SALE"
+              ? "Venta"
+              : project?.operation.type === "RENT"
+              ? "Alquiler"
+              : ""}
+          </div>
+        ) : (
+          <div className="text-sm border border-[#B0BBC5] bg-red-700 text-white py-1 px-2 w-fit rounded-lg mb-2">
+            {project?.operation.type === "SALE" ? "Vendido" : "Alquilado"}
+          </div>
+        )}
+        <PriceFormatter className="text-4xl" value={project?.price} />
+        {project?.expenses && (
+          <PriceFormatter
+            className="text-sm"
+            value={project?.expenses}
+            title="Expensas:"
+          />
+        )}
+      </div>
+    </div>
+  );
+};
 
 export default PropertyInfo;
