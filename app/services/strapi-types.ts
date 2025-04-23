93c7eba9087c4d147a21a7929b1ac201212ff4cf
Ignacio Nicolás García Poletti <polettiignacio7@gmail.com>
2025-04-23 03:01:18 -0300

    FIX | Modified stype-types file

--- app/services/strapi-types.ts
+++ app/services/strapi-types.ts
@@ -2,16 +2,16 @@
 
 export interface StrapiImage {
   url: string;
-  [key: string]: any; // Si hay más campos, ajusta aquí
+  [key: string]: unknown; // Si hay más campos, ajusta aquí
 }
 
 export interface StrapiDescriptionChild {
   text: string;
-  [key: string]: any;
+  [key: string]: unknown;
 }
 
 export interface StrapiDescriptionBlock {
   type: string;
   children: StrapiDescriptionChild[];
-  [key: string]: any;
+  [key: string]: unknown;
 }
