4dbc79f16cb28a569d9beb2c10ea1352cd628d15
Ignacio Nicolás García Poletti <polettiignacio7@gmail.com>
2025-04-25 17:38:09 -0300

    SEO | Added Structured Data to properties page

--- app/page.tsx
+++ app/page.tsx
@@ -2,8 +2,29 @@ import { HomeContainer } from "./containers";
 
 export default function Home() {
   return (
-    <main>
-      <HomeContainer/>
-    </main>
+    <>
+      <head>
+        <script
+          type="application/ld+json"
+          dangerouslySetInnerHTML={{
+            __html: JSON.stringify({
+              '@context': 'https://schema.org',
+              '@type': 'Organization',
+              name: 'Romina Frola - Agente Inmobiliario',
+              url: 'https://rfrola.com.ar/',
+              logo: 'https://rfrola.com.ar/logo.png',
+              description: 'Soy Romina Frola, asesora inmobiliaria en RE/MAX Time, especializada en ofrecer un servicio personalizado y transparente que acompaña a cada cliente en su camino hacia la compra, venta o alquiler de su propiedad ideal.',
+              sameAs: [
+                'https://www.instagram.com/rominafrola.remax/',
+                'https://www.facebook.com/rominafrola.remax/'
+              ]
+            })
+          }}
+        />
+      </head>
+      <main>
+        <HomeContainer/>
+      </main>
+    </>
   );
 }
