24d409b325d76a9ca5ca7df0617da1a5d9bb94fb
Ignacio Nicolás García Poletti <polettiignacio7@gmail.com>
2025-04-25 17:44:35 -0300

    ACCS | Added discernible name to links

--- app/components/contact-card/contact-button/index.tsx
+++ app/components/contact-card/contact-button/index.tsx
@@ -9,10 +9,10 @@ interface ContactButtonProps {
     Icon: IconType;
 }
 
-export const ContactButton: FC<ContactButtonProps> = ({value, url, Icon}) => {
+export const ContactButton: FC<ContactButtonProps> = ({name, value, url, Icon}) => {
     return (
-        <Link target="_blank" href={url} className="relative group flex flex-col items-center justify-center h-12 w-full">
-            <Icon className='absolute group-hover:opacity-0 text-5xl transition-opacity duration-300 ease-out'/>
+        <Link target="_blank" href={url} aria-label={value} className="relative group flex flex-col items-center justify-center h-12 w-full">
+            <Icon className='absolute group-hover:opacity-0 text-5xl transition-opacity duration-300 ease-out' aria-label={value} />
             <p className="hidden text-sm group-hover:flex transition-opacity duration-150 ease-in-out opacity-0 group-hover:opacity-100">
                 {value}
             </p>
