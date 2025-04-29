86d2f8bd00dc69f07b296b6990dafcf7cbedc5db
Ignacio Nicolás García Poletti <polettiignacio7@gmail.com>
2025-04-29 20:23:38 -0300

    FIX | Deleted HeroUI due to incompability

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,12 +1,10 @@
-import {heroui} from '@heroui/theme';
 import type { Config } from "tailwindcss";
 
 export default {
   content: [
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
-    "./node_modules/@heroui/theme/dist/components/spinner.js"
+    "./app/**/*.{js,ts,jsx,tsx,mdx}"
   ],
   theme: {
     extend: {
@@ -21,5 +19,4 @@ export default {
       pattern: /bg-\[url\(.*\)\]/,
     },
   ],
-  plugins: [heroui()],
 } satisfies Config;
