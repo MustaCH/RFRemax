afe42eddee8b8adac3ab0ccb0397a87e67c176b3
Nacho <polettiignacio7@gmail.com>
2025-07-16 19:01:35 -0300

    FIX | Deleted Contact Form Modal

--- app/containers/home-container/index.tsx
+++ app/containers/home-container/index.tsx
@@ -1,26 +1,13 @@
 'use client'
 
-import { ContactFormModal, WelcomeBanner } from "@/app/components";
+import { WelcomeBanner } from "@/app/components";
 import {PropertiesContainer, ContactContainer, WhoamiContainer} from "@/app/containers";
-import { useEffect, useState } from "react";
 
 
 export const HomeContainer = () => {
-    const [isModalOpen, setModalOpen] = useState(true);
-
-    useEffect(() => {
-        const hasVisited = sessionStorage.getItem("hasVisited");
-        if (!hasVisited) {
-          setModalOpen(true); 
-          sessionStorage.setItem("hasVisited", "true"); 
-        }
-      }, []);
 
     return (
-        <div>
-            {isModalOpen && (
-                <ContactFormModal onClose={() => setModalOpen(false)} />
-            )}            
+        <div>           
             <section>
                 <WelcomeBanner/>
             </section>
