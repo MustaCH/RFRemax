2eee732efbf72d7ff9104064c53d6e76e48e9185
Nacho <polettiignacio7@gmail.com>
2025-08-28 20:23:40 -0300

    FIX | Added personalized message to whatsapp url

--- app/components/property-contact-form.tsx
+++ app/components/property-contact-form.tsx
@@ -20,7 +20,7 @@ const PropertyContactForm: FC<PropertyContactFormProps> = ({ propertyTitle }) =>
 
   return (
     <div className="w-full">
-        <a href="https://api.whatsapp.com/send/?phone=%2B5491158942180&type=phone_number&app_absent=0" target="_blank" className="flex items-center justify-center gap-1 cursor-pointer hover:opacity-80 transition-all bg-green-500 rounded-full py-2 px-4 w-full" onClick={handleClick}>
+        <a href={`https://api.whatsapp.com/send/?phone=%2B5491158942180&text=Hola,%20quisiera%20m%C3%A1s%20informaci%C3%B3n%20sobre%20la%20propiedad%20${propertyTitle}&type=phone_number&app_absent=0`} target="_blank" className="flex items-center justify-center gap-1 cursor-pointer hover:opacity-80 transition-all bg-green-500 rounded-full py-2 px-4 w-full" onClick={handleClick}>
             <FaWhatsapp className="w-10 h-10 text-white"/>
             <p className="text-white font-semibold text-lg">CONTACTAME</p>
         </a>
